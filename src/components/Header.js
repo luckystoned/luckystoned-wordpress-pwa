3a74b51777f1bc53b641243b13e579a1cd1a28cc
Luckystoned <luckystoned.dev@gmail.com>
2020-11-20 00:08:24 -0300

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,66 @@
 import React from "react"
 //import PropTypes from "prop-types"
 
-const Header = ({  }) => <div>Header</div>
 
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+import Navigation from "./Navigation"
+
+import tangoLogo from "../images/tango_logo.svg"
+import { HeaderWrapper } from "./styles/HeaderStyles"
+
+const Header = () => {
+    const {
+        site,
+        menu : {
+            edges : [{ node: menu }],
+        },
+    } = useStaticQuery(graphql`
+        query HeaderQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+            menu:  allWordpressWpApiMenusMenusItems(
+              filter: { wordpress_id: { eq: 179 } } 
+            ) 
+              {
+              totalCount
+              edges {
+                node {
+                    items {
+                        url
+                        title
+                        wordpress_children {
+                            title
+                            url     
+                        }
+                    }
+                    name
+                }
+              }
+            }
+        }
+        
+        `)
+
+    return(
+        <HeaderWrapper>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <Link to="/">
+                            <img src={tangoLogo} alt={site.siteMetadata.title}/>
+                        </Link>
+                    </div>
+                    <div className="col-md-8">
+                        <Navigation menu={menu}/>
+                    </div>
+                </div>
+            </div>
+        </HeaderWrapper>
+    )
+}
 
 export default Header
---
+++ src/components/Navigation.js
@@ -0,0 +1,8 @@
+import React from "react"
+//import PropTypes from "prop-types"
+
+
+const Navigation = ({  }) => <div>Navigation</div>
+
+
+export default Navigation
