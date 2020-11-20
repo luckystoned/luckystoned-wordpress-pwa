87e46667e390da798c6bee343c244900bce45bb0
Luckystoned <luckystoned.dev@gmail.com>
2020-11-20 00:21:36 -0300

    Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,37 @@
 import React from "react"
-//import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import propTypes from "prop-types"
 
+import { NavigationWrapper } from "./styles/NavigationStyles"
 
-const Navigation = ({  }) => <div>Navigation</div>
+const Navigation = ({ menu }) => (
+    
+    <NavigationWrapper>
+        <ul>
+            {menu.items.map((item,i) => (
+                <li key={i}>
+                    <Link to={item.url} activeClassName="nav-active">
+                        {item.title}
+                    </Link>
+                    {item.wordpress_children ? (
+                        <ul>
+                            {item.wordpress_children.map((child, iChild) => (
+                                <li key={iChild}>
+                                    <Link to={child.url} activeClassName="nav-active">
+                                        {child.title}
+                                    </Link>   
+                                </li>
+                            ))}
+                        </ul>
+                    ) : null}
+                </li>
+            ))}
+        </ul>
+    </NavigationWrapper>
+)
 
+Navigation.propTypes = {
+    manu: propTypes.object,
+}
 
 export default Navigation
