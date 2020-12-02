0bf1d3da1cec4249a38abd37e2ab3aff055f4979
Luckystoned <luckystoned.dev@gmail.com>
2020-12-02 00:46:19 -0300

    CTA Images

---
+++ src/components/index/CTA.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import {
+    StyledImg,
+    CTAImage,
+    CTAImageTextWrapper,
+    CTAImageText,
+} from './styles/CTAImagesStyles'
+
+const CTA = ({ image, link, text }) => (
+    <CTAImage>
+        <StyledImg fluid={image}/>
+        <Link to={link}>
+            <CTAImageTextWrapper>
+                <CTAImageText>{text}</CTAImageText>
+            </CTAImageTextWrapper>
+        </Link>
+    </CTAImage>
+)
+
+
+export default CTA;
--- src/components/index/CTAImages.js
+++ src/components/index/CTAImages.js
@@ -1,5 +1,77 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const CTAImages = () => <div>CTAImages</div>
+import CTA from './CTA'
+
+const CTAImages = () => {
+    const {
+        cta : { acf: cta },
+    } = useStaticQuery(graphql`
+
+        fragment ctaImage on wordpress__wp_media { 
+            localFile {
+                childImageSharp {
+                    fluid(quality:100, maxWidth: 500) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+        }
+
+        query {
+            cta: wordpressPage(wordpress_id: {eq: 137}) {
+                id
+                acf {
+                    puff_1_image {
+                        ...ctaImage
+                    }
+                    puff_1_link
+                    puff_1_text
+                    puff_2_image {
+                        ...ctaImage
+                    }
+                    puff_2_link
+                    puff_2_text
+                    puff_3_image {
+                        ...ctaImage
+                    }
+                    puff_3_link
+                    puff_3_text
+                }
+            }
+        }
+
+    `)
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <CTA 
+                            image={cta.puff_1_image.localFile.childImageSharp.fluid}
+                            link={cta.puff_1_link}
+                            text={cta.puff_1_text}
+                        />
+                    </div>
+                    <div className="col-md-4">
+                        <CTA 
+                            image={cta.puff_2_image.localFile.childImageSharp.fluid}
+                            link={cta.puff_2_link}
+                            text={cta.puff_2_text}
+                        />
+                    </div>
+                    <div className="col-md-4">
+                        <CTA 
+                            image={cta.puff_3_image.localFile.childImageSharp.fluid}
+                            link={cta.puff_3_link}
+                            text={cta.puff_3_text}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default CTAImages;
