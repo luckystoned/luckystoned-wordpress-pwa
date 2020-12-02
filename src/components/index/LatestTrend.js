733af04915a49739c47db0e69d2748b69e5b72e0
Luckystoned <luckystoned.dev@gmail.com>
2020-12-02 02:03:29 -0300

    LatestTrend

--- src/components/index/LatestTrend.js
+++ src/components/index/LatestTrend.js
@@ -1,5 +1,50 @@
+/* eslint-disable react/no-danger  */
 import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import styled from 'styled-components'
 
-const LatestTrend = () => <div>LatestTrend</div>
+const LatestTrenWrapper = styled.div`
+    text-align:center;
+    margin: 40px 0;
+`
+
+const LatestTrend = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allWordpressPost(sort: {fields: date, order: DESC}) {
+                edges {
+                    node {
+                        title
+                        excerpt
+                        slug
+                    }
+                }
+            }
+        }
+    `)
+
+    return  (
+        <LatestTrenWrapper>
+            <div>
+                <div>
+                    <div>
+                        <h1>Latest Trend</h1>
+                        <h4>{data.allWordpressPost.edges[0].node.title}</h4>
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: data.allWordpressPost.edges[0].node.excerpt,
+                            }}
+                        />
+                        <Link
+                            to={`/trends/${data.allWordpressPost.edges[0].node.slug}`}
+                        >
+                            <h5>Read More</h5>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </LatestTrenWrapper>
+    )
+}
 
 export default LatestTrend;
