c2385f7f43e7b4d1928ae508f7a6b3bf0872c290
Luckystoned <luckystoned.dev@gmail.com>
2020-12-02 03:19:41 -0300

    Qoute / About

--- src/components/index/About.js
+++ src/components/index/About.js
@@ -1,5 +1,51 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const About = () => <div>About</div>
+import { AboutWrapper } from './styles/AboutStyles'
+
+const About = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            wordpressPage(wordpress_id: {eq: 10018}) {
+                content
+                featured_media {
+                    localFile {
+                        childImageSharp {
+                            fluid(quality: 100, maxWidth: 2000) {
+                                ...GatsbyImageSharpFluid_withWebp 
+                            }
+                        }
+                        absolutePath
+                    }
+                }
+            }
+        }
+    `)
+
+    return (
+        <>
+            <AboutWrapper>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div
+                            className="col-md-6 aboutImage"
+                            style={{
+                                backgroundImage: `url(${
+                                    data.wordpressPage.featured_media.localFile.childImageSharp.fluid.src
+                                })`
+                            }}
+                        />
+
+                        <div className="col-md-6 aboutText">
+                            <div
+                                dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </AboutWrapper>
+        </>
+    )
+}
 
 export default About;
--- src/components/index/Quote.js
+++ src/components/index/Quote.js
@@ -1,5 +1,36 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Quote = () => <div>Quote</div>
+import citatimg from '../../images/tango_citat.svg'
+import { CitatWrapper } from './styles/CitatStyles'
+
+const Quote = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            wordpressPage(wordpress_id: {eq: 137}) {
+                acf {
+                quote_1_text
+                quote_1_author
+                }
+            }
+        }
+    `)
+
+    return (
+        <>
+            <CitatWrapper>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <img src={citatimg} alt="quote"/>
+                            <h6>{data.wordpressPage.acf.quote_1_text}</h6>
+                            {data.wordpressPage.acf.quote_1_author}
+                        </div>
+                    </div>
+                </div>
+            </CitatWrapper>
+        </>
+    )
+}
 
 export default Quote;
