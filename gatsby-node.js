92e29cd4f56690c9ea5d54d879488464420ac3ab
Luckystoned <luckystoned.dev@gmail.com>
2021-01-06 22:44:57 -0300

    Node.js coding fot the page Template

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,49 @@
  */
 
 // You can delete this file if you're not using it
+
+
+const path = require('path');
+const slash = require('slash');
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+    const pageTemplate = path.resolve('./src/templates/page.js');
+
+    const result = await graphql(`
+        {
+            allWordpressPage {
+                edges {
+                    node {
+                        id
+                        status
+                        link
+                        wordpress_id
+                        wordpress_parent
+                    }
+                }
+            }
+        }
+    `)
+
+    //check for errors
+    if (result.errors) {
+        throw new Error(result.errors);
+    }
+
+    const { allWordpressPage } = result.data;
+
+    allWordpressPage.edges.forEach(edge => {
+        if (edge.node.status === 'publish') {
+            createPage({
+                path: edge.node.link,
+                component: slash(pageTemplate),
+                context: {
+                    id: edge.node.id,
+                    parent: edge.node.wordpress_parent,
+                    wpId: edge.node.wordpress_id,
+                },
+            });
+        }
+    });
+}
---
+++ src/templates/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PageTemaplate = () => <div>PageTemplate</div>
+
+export default PageTemaplate;
