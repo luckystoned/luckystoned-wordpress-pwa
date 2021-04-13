2ae8e8d4231a27b8f3f2b8cb888bde8a705b662f
Luckystoned <luckystoned.dev@gmail.com>
2021-04-13 19:15:12 -0300

    add post side bar

--- src/components/post/PostSidebar.js
+++ src/components/post/PostSidebar.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+/* eslint-disable react/no-danger */
+import React from 'react';
+import { Link } from 'gatsby';
 
-class PostSidebar extends Component {
-    render() {
-        return (
-            <div>
-                Post Sidebar
-            </div>
-        );
-    }
-}
+import AuthorIcon from '../../images/tango-author-icon.svg';
+import CategoryIcon from '../../images/tango-category-icon.svg';
+import DateIcon from '../../images/tango-date-icon.svg';
+
+import { SidebarWrapper, SidebarMenu } from './styles/PostSidebarStyles';
+
+const PostSidebar = ({ date, author, categories }) => (
+  <SidebarWrapper className="col-lg-3">
+    <SidebarMenu>
+      <li className="sidebar-menu-header">
+        <img src={DateIcon} alt="tango-date" />
+        <span>{date}</span>
+      </li>
+      <li className="sidebar-menu-header">
+        <img src={AuthorIcon} alt="tango-author" />
+        <span>{author}</span>
+      </li>
+      <li className="sidebar-menu-header">
+        <img src={CategoryIcon} alt="tango-category" />
+        <span>Categories</span>
+      </li>
+      {categories.map(cat =>
+        cat.slug !== 'alla-trendspaningar' ? (
+          <li key={cat.id}>
+            <Link to={`/trends/${cat.slug}/`}>
+              <span dangerouslySetInnerHTML={{ __html: cat.name }} />
+            </Link>
+          </li>
+        ) : null
+      )}
+    </SidebarMenu>
+  </SidebarWrapper>
+);
 
 export default PostSidebar;
